020177b58e9a278fea17623268aa77e4b38cc587
stephenyau <softwareentrepreneer@gmail.com>
2024-02-17 14:49:44 +0800

    draft

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,29 @@
 export default {
-    logo: <span>My Nextra Documentation</span>,
+    logo: (
+      <span style={{ fontWeight: 'bold', fontFamily: 'Futura', fontSize: '28px' }}>
+        <text style={{color: '#306998'}}>
+          Py
+        </text>
+        <text style={{color: '#FFD43B'}}>
+          Trade
+        </text>
+      </span>
+    ),
+    faviconGlyph: "PT",
     project: {
-      link: 'https://github.com/shuding/nextra'
-    }
+      link: 'https://github.com/PFund-Software-Ltd/pytrade.org'
+    },
+    feedback: {
+        content: null,  // disable "Question? Give us feedback →"
+    },
+    docsRepositoryBase: 'https://github.com/PFund-Software-Ltd/pytrade.org',
+    sidebar: {
+        toggleButton: true,
+    },
+    footer: {
+        component: null,  // disable footer
+    },
+    useNextSeoProps: () => ({
+    }),
     // ... other theme options
   }
