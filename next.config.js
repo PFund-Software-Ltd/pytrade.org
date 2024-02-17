811a9d81bc957eea985c0ab01b2c8c1b8f178f54
stephenyau <softwareentrepreneer@gmail.com>
2024-02-17 15:17:53 +0800

    update nextjs config for redirects

--- next.config.js
+++ next.config.js
@@ -1,9 +1,21 @@
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://docs.pytrade.org',
+        permanent: true,
+      },
+    ]
+  },
+}
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx'
 })
  
-module.exports = withNextra()
+module.exports = withNextra(nextConfig)
  
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })
