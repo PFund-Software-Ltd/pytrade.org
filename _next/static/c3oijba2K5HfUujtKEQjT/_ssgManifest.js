28a292e3e1380520e053c4b543388c4f6cd33273
softwareentrepreneer <softwareentrepreneer@users.noreply.github.com>
2024-02-22 10:26:53 +0000

    deploy: 9210c4be3aa81528ad86e20344dee0437616587d

---
+++ _next/static/c3oijba2K5HfUujtKEQjT/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fanalytics","\u002Fbacktest","\u002Fdata_libs","\u002Fdata_tools","\u002Fresearch","\u002Ftrading"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
